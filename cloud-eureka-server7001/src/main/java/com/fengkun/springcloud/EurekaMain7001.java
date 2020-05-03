d743979c3ef10babcd136e93b0fa5c3af1c4e539
fengkunangel <fengkunangel@outlook.com>
2020-05-03 18:23:33 +0800

    加入eureka

--- cloud-eureka-server7001/src/main/java/com/fengkun/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/fengkun/springcloud/EurekaMain7001.java
@@ -2,17 +2,17 @@ package com.fengkun.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author : fengkun
- * @date : 2020/5/2
+ * @date : 2020/5/3
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain8080 {
+@EnableEurekaServer
+public class EurekaMain7001 {
 
   public static void main(String[] args) {
-    SpringApplication.run(OrderMain8080.class, args);
+    SpringApplication.run(EurekaMain7001.class, args);
   }
 }
--- cloud-provider-payment8001/src/main/java/com/fengkun/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/fengkun/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.fengkun.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author : fengkun
  * @date : 2020/5/2
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
 
   public static void main(String[] args) {
